bc3a96d1bec35e54343b42f7b7069deaffae7717
SahinoorHUB <sahinoormd1996@gmail.com>
2022-05-06 09:28:06 +0530

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngForm-validation';
+export class AppComponent implements OnInit {
+  Userform!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.Userform = this.formBuilder.group({ 
+        username: [ '', [Validators.required]],
+        password: ['',[Validators.required,Validators.minLength(6),Validators.maxLength(40)]], 
+      });
+  }
+
+  get formCheck(): { [key: string]: AbstractControl } {
+    return this.Userform.controls;
+  }
+
+  submitData(): void { 
+    if (this.Userform.invalid) {
+      this.submitted = true;
+      return;
+    } else {
+      console.log(this.Userform.value); 
+    }
+
+     
+  }
+
+   
 }
